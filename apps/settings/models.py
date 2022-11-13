90a19cc39ac63e3c6c20d16b78532b874cfce45c
alinu1 <uwu1343@gmail.com>
2022-11-13 12:12:26 +0600

    add template and write first models & func

---
+++ apps/settings/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Setting(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Название сайта")
+    description = models.TextField(verbose_name="Описание сайта")
+    logo = models.ImageField(upload_to = 'logo/', verbose_name="Логотип сайта")
+    second_logo= models.ImageField(upload_to= 'second_logo/', verbose_name="second logo")
+    phone = models.CharField(max_length=255, verbose_name="Телефонный номер")
+    email = models.EmailField(max_length=150, verbose_name="Электронная почта сайта")
+    facebook = models.URLField(blank = True, null=True, verbose_name="Ссылка на facebook")
+    youtube = models.URLField(blank = True, null=True, verbose_name="Ссылка на youtube")
+    instagram = models.URLField(blank = True, null=True, verbose_name="Ссылка на instagram")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Настройка"
+        verbose_name_plural = "Настройки"
