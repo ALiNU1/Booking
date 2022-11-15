49b52548d6c3d240073fe9bf95ca9844a2ae50ad
alinu1 <uwu1343@gmail.com>
2022-11-15 20:33:31 +0600

    configured header

--- apps/cruises/admin.py
+++ apps/cruises/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Cruises,CruisePhoto
 
 # Register your models here.
+admin.site.register(Cruises)
+admin.site.register(CruisePhoto)
+
+class CruisesoAdmin(admin.TabularInline):
+    model= Cruises
+    extra = 5
+class CruisePhotoAdmin(admin.TabularInline):
+    model= CruisePhoto
+    extra = 5
+class CruisePhotos(admin.ModelAdmin):
+    inlines = [CruisePhoto, Cruises]
--- apps/cruises/models.py
+++ apps/cruises/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Cruises(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    poster = models.ImageField('cruiese/')
+    price = models.BigIntegerField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Круиз'
+        verbose_name_plural = 'Круизы'
+
+class CruisePhoto(models.Model):
+    cruise = models.ForeignKey(Cruises,on_delete=models.CASCADE,related_name="cruisephoto",null = True,blank = True)
+    photo = models.ImageField(upload_to='cruises/')
+
+
+    class Meta:
+        verbose_name = "Круиз фото"
+        verbose_name_plural = "Фото круиза"
