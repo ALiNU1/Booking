165cc291818cd0576fc9585a8a56add820acb207
alinu1 <uwu1343@gmail.com>
2022-11-17 14:32:03 +0600

    register

--- apps/users/models.py
+++ apps/users/models.py
@@ -5,8 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     phone = models.CharField(max_length=100)
     email = models.EmailField()
-    profile_image = models.FileField(upload_to='users/')
-    url = models.URLField(blank=True,null=True)
+    
 
     def __str__ (self):
         return self.username
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from apps.settings.models import Setting
+from .models import User
+from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
+def register(request):
+    setting = Setting.objects.latest('id')
+    if request.method == "POST":
+        username = request.POST.get('username')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        if password1 == password2:
+            user = User.objects.create(username = username,first_name = first_name, last_name = last_name,  email = email)
+            user.set_password(password1)
+            user.save()
+            user = User.objects.get(username = username)
+            user = authenticate(username = username, password = password1)
+            login(request, user)
+            return redirect('index')
+           
+        else:
+            return HttpResponse("Пароли не совпадают")
+
+    context = {
+        'setting' : setting,
+    }
+    return render(request, 'booking/register.html', context)
+
+def profile(request):
+    setting = Setting.objects.latest('id')
+    user = User.objects.all()
+     
+    context = {
+        'setting' : setting,
+        'user' : user,
+    }
+    return render(request,'booking/my_account.html', context)
