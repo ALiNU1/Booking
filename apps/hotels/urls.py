8c2854db6357cb91782b4165f24a2d67da1650bd
alinu1 <uwu1343@gmail.com>
2022-11-17 16:59:11 +0600

    search

--- apps/hotels/urls.py
+++ apps/hotels/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import hotel_details
+from .views import hotel_details,hotel_search
 
 urlpatterns = [ 
     path('hotel_details/<int:id>/',hotel_details,name='hotel_details'),
+    path('search/',hotel_search,name="search"),
 ]
--- apps/hotels/views.py
+++ apps/hotels/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from apps.settings.models import Setting
 from .models import Hotels,HotelImage
+from django.db.models import Q
 
 # Create your views here.
 def hotel_details(request, id):
@@ -14,3 +15,16 @@ def hotel_details(request, id):
         'hotel_image' : hotel_image,
     }
     return render(request,'booking/hotel.html', context)
+
+def hotel_search(request):
+    setting = Setting.objects.latest('id')
+    hotels = Hotels.objects.all()
+    qury_object = request.GET.get('key')
+    if qury_object:
+        hotels = Hotels.objects.filter(Q(name__icontains = qury_object))
+    context = {
+        'setting' : setting,
+        'hotels' : hotels,
+        
+    }
+    return render(request,"booking/search_results.html", context)
